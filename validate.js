933a56beb97bf506a4ad2be4b71695d8fd04f775
vharsh43 <vharsh43@gmail.com>
2020-04-12 01:06:06 +0530

    update
    Co-Authored-By: sgd476 <sgd476@users.noreply.github.com>

--- validate.js
+++ validate.js
@@ -274,7 +274,3 @@ function NoteForm(){
 
 
 /***************************************************************************** */
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    alert("For a good navigation, please put the page in full screen with the button 'change view'");
-});
